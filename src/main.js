9b383c6218df39d39cc3c3078268b7d10a44e350
karim-bouchez <bouchez.karim@gmail.com>
2022-12-31 18:13:21 +0100

    kill child process on quit

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ if (require("electron-squirrel-startup")) {
     app.quit();
 }
 
-app.on("quit", (_) => tearDown());
+app.on("before-quit", async () => await tearDown());
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- src/main/setup.js
+++ src/main/setup.js
@@ -1,4 +1,7 @@
 const { spawn } = require("child_process");
+const treeKill = require("tree-kill");
+const util = require("util");
+const kill = util.promisify(treeKill);
 
 const initIpcHandlers = require("./ipc-handlers");
 const {
@@ -12,13 +15,15 @@ const {
 } = require("./const");
 const LibraryService = require("./services/library");
 const { ping: pingMusicRemover } = require("./lib/music-remover");
+const { sleep } = require("./helpers.js");
 const config = require(CONFIG_PATH);
 
+let musicRemoverProcess;
+
 function startMusicRemoverProcess() {
-    let backendProcess;
     if (IS_APPLE_SILICON) {
         try {
-            backendProcess = spawn(
+            musicRemoverProcess = spawn(
                 "arch",
                 [
                     "-x86_64",
@@ -41,32 +46,50 @@ function startMusicRemoverProcess() {
         }
     } else {
         const exec_name = "./app" + EXEC_EXTENSION;
-        backendProcess = spawn(exec_name, { cwd: RESOURCE_DIR });
+        musicRemoverProcess = spawn(exec_name, { cwd: RESOURCE_DIR });
     }
 
-    backendProcess.stdout.on("data", (data) => {
+    musicRemoverProcess.stdout.on("data", (data) => {
         console.log(`stdout: ${data}`);
     });
 
-    backendProcess.stderr.on("data", (data) => {
+    musicRemoverProcess.stderr.on("data", (data) => {
         console.error(`stderr: ${data}`);
     });
 
-    backendProcess.on("close", (code) => {
+    musicRemoverProcess.on("close", (code) => {
         console.log(`child process exited with code ${code}`);
     });
+    console.log(musicRemoverProcess.pid);
 }
 
 async function setUp() {
     initIpcHandlers();
     startMusicRemoverProcess();
-    pingMusicRemover({ recursive: true }).then(() => {
+    pingMusicRemover({ recursive: true }).then(async () => {
         console.log("gRPC server UP!");
         LibraryService.initQueue();
+
+        // console.log("Killing 'musicRemoverProcess'...");
+        // await sleep(3000);
+        // console.log(musicRemoverProcess.pid);
+        // kill(musicRemoverProcess.pid, function (err) {
+        //     if (err) console.error({ err });
+        // });
+        // console.log("DONE 'musicRemoverProcess'...");
     });
 }
 
-function tearDown() {}
+async function tearDown() {
+    if (musicRemoverProcess) {
+        try {
+            await kill(musicRemoverProcess.pid, "SIGTERM");
+            console.log("'musicRemoverProcess' killed");
+        } catch (error) {
+            console.error({ error });
+        }
+    }
+}
 
 module.exports = {
     setUp,
