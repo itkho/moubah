f878991a078d20726c4f17e64bceac270a4fe93c
karim-bouchez <bouchez.karim@gmail.com>
2023-01-31 20:02:59 +0100

    truncate text on preview

--- renderer/components/PreviewModal.tsx
+++ renderer/components/PreviewModal.tsx
@@ -16,16 +16,18 @@ export default function PreviewModal({
     return (
         <>
             <div className="fixed inset-0 z-50 flex items-center justify-center">
-                <div className=" flex-col rounded-lg bg-neutral-200 shadow-lg">
+                <div className=" w-3/4 flex-col rounded-lg bg-neutral-200 shadow-lg">
                     {/* Header */}
-                    <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-300 p-5">
-                        <div>
-                            <h3 className="font-semibold">{video.title}</h3>
+                    <div className="flex w-full items-start justify-between rounded-t border-b border-solid border-slate-300 p-5">
+                        <div className="overflow-hidden">
+                            <h3 className="truncate font-semibold">
+                                {video.title + video.title}
+                            </h3>
                             <p>{video.author.name}</p>
                             <p>{abbrNum(video.views)} views</p>
                         </div>
                         <button
-                            className="p-1 text-2xl text-neutral-400"
+                            className="p-5 text-2xl text-neutral-400"
                             onClick={() => setShowModal(false)}
                         >
                             ×
