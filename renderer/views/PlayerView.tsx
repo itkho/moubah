b6f2d66488d08b87a6046e965ea082841e79f4e1
karim-bouchez <bouchez.karim@gmail.com>
2023-02-08 08:21:53 +0100

    improve player

--- renderer/views/PlayerView.tsx
+++ renderer/views/PlayerView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { KeyboardEventHandler, RefObject, useRef } from "react";
 import ReactPlayer from "react-player";
 import PlayerPlaceHolder from "../components/PlayerPlaceHolder";
 
@@ -10,24 +10,55 @@ export default function PlayerView({ hidden }: { hidden: boolean }) {
     console.log("PlayerView rendered!");
 
     const { video } = usePlayer();
+    const player = useRef<ReactPlayer>(null);
+
+    const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+        console.log("onKeyDown");
+        let shift = 0;
+        switch (event.key) {
+            case "ArrowRight":
+                shift = +5;
+                break;
+            case "ArrowLeft":
+                shift = -5;
+                break;
+            default:
+                return;
+        }
+        console.log(shift);
+        player.current?.seekTo(
+            player.current?.getCurrentTime() + shift,
+            "seconds"
+        );
+    };
 
     return (
         <>
             {!hidden && (
                 <>
                     {video?.videoUri ? (
-                        <div className="p-10">
-                            <div>{video?.title}</div>
-                            <div>{video?.author?.name}</div>
-                            {/* <ReactPlayer
-                                controls
-                                volume={0}
-                                url="https://www.youtube.com/watch?v=XqpFCuPAEPo"
-                            /> */}
-                            <ReactPlayer
-                                controls
-                                url={cleanSrcPath(video?.videoUri)}
-                            />
+                        <div className="flex flex-col p-10">
+                            <div className="my-4">
+                                <h1 className="text-3xl font-semibold">
+                                    {video?.title}
+                                </h1>
+                                <h3 className="text-xl font-light">
+                                    {video?.author?.name}
+                                </h3>
+                            </div>
+                            {/* TODO: https://code.pieces.app/blog/developing-a-react-video-player-with-personalized-controls#:~:text=Watch%20Now-,Handling%20the%20Rewind%20and%20Fast%20Forward%20functionalities,-. */}
+                            <div
+                                className="overflow-hidden rounded-lg"
+                                onKeyDown={onKeyDown}
+                            >
+                                <ReactPlayer
+                                    ref={player}
+                                    controls
+                                    url={cleanSrcPath(video?.videoUri)}
+                                    width="100%"
+                                    height="100%"
+                                />
+                            </div>
                         </div>
                     ) : (
                         <PlayerPlaceHolder />
