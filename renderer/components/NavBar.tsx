57f4ef5bc19c979f028480239882390319ce900a
karim-bouchez <bouchez.karim@gmail.com>
2023-02-15 08:39:21 +0100

    add triangle

--- renderer/components/NavBar.tsx
+++ renderer/components/NavBar.tsx
@@ -118,54 +118,67 @@ export default function NavBar() {
                                     />
                                 </Listbox.Button>
                                 <Transition
-                                    as={Fragment}
+                                    as="div"
+                                    // as={Fragment}
                                     leave="transition ease-in duration-100"
                                     leaveFrom="opacity-100"
                                     leaveTo="opacity-0"
                                 >
-                                    <Listbox.Options className="absolute bottom-full z-10 mb-4 w-full overflow-auto rounded-md bg-base-100-light py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5">
-                                        {Object.values(Locale).map((value) => {
-                                            const Flag = Flags[value];
-                                            return (
-                                                <Listbox.Option
-                                                    key={value}
-                                                    className={({ active }) =>
-                                                        `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
-                                                            active
-                                                                ? "bg-lime-100 text-lime-900"
-                                                                : "text-base-800-light"
-                                                        }`
-                                                    }
-                                                    value={value}
-                                                >
-                                                    {({ selected }) => (
-                                                        <>
-                                                            <span
-                                                                className={`flex items-center truncate ${
-                                                                    selected
-                                                                        ? "font-medium"
-                                                                        : "font-normal"
-                                                                }`}
+                                    <div className="absolute bottom-full z-10 mb-4 w-full rounded-md bg-base-100-light py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-base-500-light">
+                                        <div className="relative">
+                                            <Listbox.Options>
+                                                {Object.values(Locale).map(
+                                                    (value) => {
+                                                        const Flag =
+                                                            Flags[value];
+                                                        return (
+                                                            <Listbox.Option
+                                                                key={value}
+                                                                className={({
+                                                                    active,
+                                                                }) =>
+                                                                    `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                                                                        active
+                                                                            ? "bg-lime-100 text-lime-900"
+                                                                            : "text-base-800-light dark:text-base-200-light"
+                                                                    }`
+                                                                }
+                                                                value={value}
                                                             >
-                                                                <Flag className="mr-2 h-4 shrink-0 rounded" />
-                                                                {langFromLocale(
-                                                                    value
+                                                                {({
+                                                                    selected,
+                                                                }) => (
+                                                                    <>
+                                                                        <span
+                                                                            className={`flex items-center truncate ${
+                                                                                selected
+                                                                                    ? "font-medium"
+                                                                                    : "font-normal"
+                                                                            }`}
+                                                                        >
+                                                                            <Flag className="mr-2 h-4 shrink-0 rounded" />
+                                                                            {langFromLocale(
+                                                                                value
+                                                                            )}
+                                                                        </span>
+                                                                        {selected && (
+                                                                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-lime-600">
+                                                                                <CheckIcon
+                                                                                    className="h-5 w-5"
+                                                                                    aria-hidden="true"
+                                                                                />
+                                                                            </span>
+                                                                        )}
+                                                                    </>
                                                                 )}
-                                                            </span>
-                                                            {selected && (
-                                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-lime-600">
-                                                                    <CheckIcon
-                                                                        className="h-5 w-5"
-                                                                        aria-hidden="true"
-                                                                    />
-                                                                </span>
-                                                            )}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            );
-                                        })}
-                                    </Listbox.Options>
+                                                            </Listbox.Option>
+                                                        );
+                                                    }
+                                                )}
+                                            </Listbox.Options>
+                                            <div className="clip-path-triangle absolute -bottom-[10px] left-1/2 h-3 w-3 -translate-x-1/2 -rotate-45 bg-base-100-light shadow-lg ring-1 ring-black dark:bg-base-500-light"></div>
+                                        </div>
+                                    </div>
                                 </Transition>
                             </div>
                         </Listbox>
